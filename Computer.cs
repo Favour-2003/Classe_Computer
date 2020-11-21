640a3511652b24ae6764cac93121c313d83fbbc1
Favour <favour.essien@isii.it>
2020-11-21 16:00:02 +0100

    Add project files.

---
+++ Computer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe__
+{
+    class Computer
+    {
+        protected string name;
+        protected float prezzo;
+
+        public Computer(string n, float p)
+        {
+            name   =  n;
+            prezzo = p;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public float getPrezzo()
+        {
+            return prezzo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classe__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer Acer = new Computer("Acer 5000", 500);
+            Console.WriteLine("Nome > " + Acer.getName().ToString());
+            Console.WriteLine("Prezzo > " + Acer.getPrezzo().ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
